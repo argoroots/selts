b86b55819b39d6df0262d6618fcafd6023a267f6
Argo Roots <argo@roots.ee>
2017-04-12 11:26:20 +0300

    Entu CMS

---
+++ source/script.js
@@ -0,0 +1,27 @@
+$(function() {
+    $(window).on('resize', function () {
+        var height = window.innerHeight
+
+        if (window.innerWidth < 767 || window.location.pathname !== '/') {
+            $('#header').css('margin-top', '50px')
+        } else {
+            $('#header').css('margin-top', height / 3 + 'px')
+        }
+
+        if (window.innerWidth < 767) {
+            $('#frontpage-navigation, #navigation').removeClass('list-inline')
+        } else {
+            $('#frontpage-navigation, #navigation').addClass('list-inline')
+        }
+
+        $('#content').css('margin-bottom', height + 'px')
+    }).resize()
+
+    $(window).on('scroll', function () {
+
+    }).scroll()
+
+    $('#footer').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 200)
+    })
+})
