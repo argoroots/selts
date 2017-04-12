8d7031ce2d1067eeb4e92f134de089016a3625da
Argo Roots <argo@roots.ee>
2017-04-12 18:11:58 +0300

    Use fullpage JS

--- source/script.js
+++ source/script.js
@@ -1,33 +1,11 @@
 $(function() {
-    $(window).on('resize', function () {
-        var height = window.innerHeight
-
-        if (window.innerWidth < 767 || window.location.pathname !== '/') {
-            $('#header').css('margin-top', '50px')
-        } else {
-            $('#header').css('margin-top', height / 3 + 'px')
-        }
-
-        if (window.innerWidth < 767) {
-            $('#frontpage-navigation, #navigation').removeClass('list-inline')
-        } else {
-            $('#frontpage-navigation, #navigation').addClass('list-inline')
-        }
-
-        $('#content').css('margin-bottom', height + 'px')
-        $('.news-img').css('height', height + 'px')
-    }).resize()
-
-    if ($('.news-img').length > 0) {
-        $(window).on('scroll', function () {
-            if ($(window).scrollTop() < $('#content').position().top + $('#content').height()) {
-                $('#news-images').css('top', $(window).scrollTop() + 'px')
-                console.log($(window).scrollTop())
-            }
-        }).scroll()
+    if ($('body').css('background-image') === 'none') {
+        $('body').css('background-image', "url('/assets/images/bg/" + Math.ceil(Math.random() * 8) + ".jpg')")
     }
 
-    $('#footer').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 200)
+    $('#fullpage').fullpage({
+        scrollOverflow: true,
+        navigation: true,
+        navigationPosition: 'right',
     })
 })
