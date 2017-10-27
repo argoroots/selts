db15acae1bf50a4230e83cbef0c4a24687672f06
Argo Roots <argo@roots.ee>
2017-10-27 15:44:53 +0300

    Use source folder

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # set -o errexit -o nounset
 
+
 export SOURCE_DIR=./source
-export TMP_SOURCE_DIR=./tmp_source
 export BUILD_DIR=./build
 
 export ENTU_DB=kunda
@@ -11,16 +11,8 @@ export ENTU_DB=kunda
 # export ENTU_KEY=
 
 
-# before_script:
 echo
 echo --------- PREFETCH
-rm -rf ${TMP_SOURCE_DIR}
-mkdir ${TMP_SOURCE_DIR}
-cp -r ${SOURCE_DIR}/* ${TMP_SOURCE_DIR}
-
-rm -rf ${BUILD_DIR}
-mkdir ${BUILD_DIR}
-
 rm -rf ${BUILD_DIR}/assets
 mkdir -p ${BUILD_DIR}/assets
 cp -r ./assets/* ${BUILD_DIR}/assets
@@ -29,12 +21,11 @@ rm -r node_modules
 npm install entu-ssg
 
 
-# script:
 echo
 echo --------- FETCH
 export ENTU_TYPE=news
 # export ENTU_PARENT=743
-./node_modules/entu-ssg/helpers/entu2yaml.js ${TMP_SOURCE_DIR}/uudised/_uudis/data.yaml
+./node_modules/entu-ssg/helpers/entu2yaml.js ${SOURCE_DIR}/uudised/_uudis/data.yaml
 
 
 echo
@@ -44,7 +35,6 @@ echo --------- BUILD
 
 echo
 echo --------- PICTURES
-export TMP_SOURCE_DIR=./tmp_source
-export PICTURES_YAML=${TMP_SOURCE_DIR}/uudised/_uudis/data.yaml
+export PICTURES_YAML=${SOURCE_DIR}/uudised/_uudis/data.yaml
 export PICTURES_DIR=${BUILD_DIR}
 node ./pictures.js
